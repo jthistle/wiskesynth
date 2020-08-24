a868541d4b22edd8543235a740d4b380cde1e3e4
James Thistlewood <jamesthistlewood@gmail.com>
2020-08-24 11:33:31 +0100

    respect offset values, add basic low pass filter implementation

--- wiske/note.py
+++ wiske/note.py
@@ -1,12 +1,12 @@
 
-from math import ceil
+from math import ceil, pi
 import struct
 import time
 
 from .repitch import cents_to_ratio
 from .sf2.definitions import SFGenerator, LoopType
 from .interface import CustomBuffer
-from .sf2.convertors import timecents_to_secs, decibels_to_atten
+from .sf2.convertors import timecents_to_secs, decibels_to_atten, cents_to_hertz
 from .envelope import Envelope
 from .util.logger import logger
 
@@ -45,12 +45,23 @@ class Note:
         offset_s = self.gens[SFGenerator.startAddrsOffset] + self.gens[SFGenerator.startAddrsCoarseOffset] * COARSE_SIZE
         offset_e = self.gens[SFGenerator.endAddrsOffset] + self.gens[SFGenerator.endAddrsCoarseOffset] * COARSE_SIZE
 
-        self.sample_data = struct.unpack("<{}h".format(len(sample.data) // 2), sample.data)
+        loop_offset_s = self.gens[SFGenerator.startloopAddrsOffset] + self.gens[SFGenerator.startloopAddrsCoarseOffset] * COARSE_SIZE
+        loop_offset_e = self.gens[SFGenerator.endloopAddrsOffset] + self.gens[SFGenerator.endloopAddrsCoarseOffset] * COARSE_SIZE
+        loop_offset_s -= offset_s
+        loop_offset_e -= offset_s
+
+        self.sample_data = struct.unpack(
+            "<{}h".format(self.get_data_size(sample.data, offset_s, offset_e) // 2),
+            self.frame_sample_data(sample.data, offset_s, offset_e)
+        )
         self.sample_size = len(self.sample_data)
 
         self.loop = None
         if self.gens[SFGenerator.sampleModes].loop_type in (LoopType.CONT_LOOP, LoopType.KEY_LOOP):
-            self.loop = [x for x in self.sample.loop]
+            self.loop = [
+                self.sample.loop[0] + loop_offset_s,
+                self.sample.loop[1] + loop_offset_e,
+            ]
 
         self.vol_env = Envelope(
             timecents_to_secs(self.gens[SFGenerator.delayVolEnv]),
@@ -64,6 +75,13 @@ class Note:
         self.channel_ratio = 2      # TODO do this properly
         self.single_sample_len = SINGLE_SAMPLE_LEN
 
+        # LOW PASS cutoff
+        self.init_filter_fc = cents_to_hertz(self.gens[SFGenerator.initialFilterFc])
+
+        self.cutoff_freq = self.init_filter_fc
+        self.recalculate_cutoff()
+        self.last_val = 0
+
         # Optional debug:
         # print("gens")
         # for g in self.gens:
@@ -74,6 +92,26 @@ class Note:
         #     print(">",m)
 
         # print("\nsample:", self.sample)
+
+    def recalculate_cutoff(self):
+        self.cutoff_time_const = 1 / (2 * pi * self.cutoff_freq)
+        self.cutoff_alpha = SINGLE_SAMPLE_LEN / (SINGLE_SAMPLE_LEN + self.cutoff_time_const)
+
+    def frame_sample_data(self, data, offset_s, offset_e):
+        if offset_e == 0:
+            return data[offset_s:]
+        elif offset_e > 0:  # hack - todo handle this properly
+            return data[offset_s:]
+        elif offset_e < 0:
+            return data[offset_s:offset_e]
+
+    def get_data_size(self, data, offset_s, offset_e):
+        if offset_e == 0:
+            return len(data) - offset_s
+        elif offset_e > 0:  # hack - todo handle this properly again
+            return len(data) - offset_s
+        elif offset_e < 0:
+            return len(data) - offset_s + offset_e
 
     def play(self):
         if not self.sample.is_mono:
@@ -86,9 +124,15 @@ class Note:
         self.vol_env.release()
 
     def collect(self, size, looping):
+        """
+        This function is extremely time sensitive, especially inside the while loop.
+        Anything goes in terms of optimization. Even a tiny change can make a significant
+        difference. Maintainability and clean code is secondary to performance here.
+        """
         if self.vol_env.finished:
             self.inter.end_loop(self.playback)  # TODO thread this?
-            return []
+            # also, this no longer actually sets the buffer to 'finished'. Fix this.
+            return
 
         channel_ratio = self.channel_ratio
         rate = self.total_ratio
@@ -110,8 +154,14 @@ class Note:
         loop_s = loop[0]
         loop_e = loop[1]
 
+        to_int = int   # this cuts a tiny sliver of time off the total running time
+
+        # last_raw = self.last_val_raw
+        last = self.last_val
+        cutoff_alpha = self.cutoff_alpha
+        reverse_cutoff_alpha = 1 - cutoff_alpha
         while (looping or position < end) and count < size:
-            i = int(position)
+            i = to_int(position)
             frac = position - i
             s1 = data[i]
             # If adding the offset overshoots the end of the sample loop, make sure that we wrap back arround
@@ -119,9 +169,12 @@ class Note:
             s2 = data[i + offset if not looping or i + offset < loop_e else loop_s + (i + offset - loop_e)]
             val = (s1 + (s2 - s1) * frac) * ve_current_val
 
-            yield val
+            with_filter = cutoff_alpha * val + reverse_cutoff_alpha * last
+            last = with_filter
+
+            yield with_filter
             if two_ratio:
-                yield val
+                yield with_filter
             count += channel_ratio
 
             position += rate
@@ -138,5 +191,6 @@ class Note:
                     ve_current_val = ve_start_val + (ve_target_val - ve_start_val) * (ve_position / ve_total_time)
 
         self.position = position
+        self.last_val = last
 
         vol_env.update_vals((ve_phase, ve_position, ve_start_val, ve_current_val, ve_target_val, ve_total_time))
